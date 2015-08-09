54034c94483dbe94a47ed1983f17462ba4790e66
David Bennett <david.bennett@percona.com>
2015-08-09 01:33:17 +0000

    Added build script for Ubuntu 14.x

---
+++ scripts/build_ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Build linkbenchX on Ubuntu 14.x 
+
+##################
+# installation
+##################
+
+# install dependencies
+
+sudo apt update
+sudo apt install -y wget git openjdk-7-jdk maven
+
+# make sure we have everything we need
+
+MISSING=0
+function check_if_missing {
+  if ! hash $1 2>/dev/null; then
+    echo "ERROR: $1 command required for this script."
+    MISSING=$((MISSING+1))
+  fi
+}
+for CMD in "sudo" "wget" "git" "java" "javac" "mvn"
+do
+  check_if_missing $CMD
+done
+if [ $MISSING \> 0 ]; then
+  exit 1;
+fi
+
+# download and build linkbenchX
+
+cd ~
+git clone https://github.com/vadimtk/linkbenchX.git
+cd linkbenchX
+mvn clean package -DskipTests
